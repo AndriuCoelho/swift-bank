e9123a21dce98885988e3a89e7ca337d3130c5d3
Ândriu <andriu_coelho1993@hotmail.com>
2024-01-30 14:51:09 -0300

    integrating SBLoan module with main project

---
+++ SwiftBank/Networking/Base/RequestMethod.swift
@@ -0,0 +1,16 @@
+//
+//  RequestMethod.swift
+//  SwiftBank
+//
+//  Created by Alura on 03/01/24.
+//
+
+import Foundation
+
+enum RequestMethod: String {
+    case delete = "DELETE"
+    case get = "GET"
+    case patch = "PATCH"
+    case post = "POST"
+    case put = "PUT"
+}
